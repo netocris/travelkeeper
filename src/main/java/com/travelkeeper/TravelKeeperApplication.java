0330859ab9b67c04abc69e5b5cd677ee3ed4ee5d
Cristóvão Neto <netocris@gmail.com>
2018-08-24 14:53:16 +0100

    add project struture

---
+++ src/main/java/com/travelkeeper/TravelKeeperApplication.java
@@ -0,0 +1,12 @@
+package com.travelkeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelKeeperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/travelkeeper/TravelKeeperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.travelkeeper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TravelKeeperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
